b15a760efa18f0420af207b0e03bc3314466e450
kingsizo666 <kingsizo666@gmail.com>
2025-04-18 23:10:21 +0200

    click function and textinput function

--- bot.py
+++ bot.py
@@ -6,10 +6,34 @@ import numpy as np
 import random
 import win32api, win32con
 
-#Sleep
-time.sleep(1)
+# initial sleep
+time.sleep(2)
 
-#Move to location then click
-pyautogui.moveTo(200,600)
-time.sleep(1)
-pyautogui.click()
+##
+def click(x,y):
+    """
+    Clicks on a location on the screen
+
+    :param x: x-coordinate of the location
+    :param y: y-coordinate of the location
+    """
+    pyautogui.moveTo(x,y)
+    pyautogui.mouseDown(button='left')
+    time.sleep(0.1)
+    pyautogui.mouseUp(button='left')
+
+def textinput(s):
+    """
+    Enter the provided string char by char with
+    random intervals between them
+
+    :param s: string to be inputed
+    """
+    for char in s:
+        pyautogui.write(char)
+        time.sleep(random.uniform(0.03, 0.05))
+    pyautogui.press('enter')
+
+click(290,980)
+textinput("/join battleon-9989")
+time.sleep(2)
